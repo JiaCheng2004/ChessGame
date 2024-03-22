d2e4b3acb59e327836cfc337689b02468f87f078
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2024-03-22 00:29:19 -0700

    added getIndex

--- src/chess.cpp
+++ src/chess.cpp
@@ -13,3 +13,27 @@ Chessboard newBoard() {
     }};
     return board;
 }
+
+std::pair<int, int> getIndex(std::string& square) {
+    // Check if input is size two
+    if (square.length() != 2)
+        throw std::range_error("A-H, 1-8");
+
+    // Change input to all lowercase
+    std::transform(square.begin(), square.end(), square.begin(), ::toupper);
+
+    // Calculate the rows index
+    int row = square[0] - 'A';
+
+    // Calculate the columns index
+    int column = (int)square[1];
+
+    // if the rows and columns are valid
+    if (row >= 0 && row <= 8 && column > 0 && column < 9){
+        return std::make_pair(row, column);
+    } else {
+        // else throw range error and exit
+        throw std::range_error("A-H, 1-8");
+        exit(EXIT_FAILURE);
+    }
+}
