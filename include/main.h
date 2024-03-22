338c8d93beaf8f6e4cabdc299a7abf07ecda6004
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2024-03-21 23:33:02 -0700

    added newBoard

--- include/main.h
+++ include/main.h
@@ -1,6 +1,24 @@
+#pragma once
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
 #include <../include/chess.h>
 #include <iostream>
 #include <unistd.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <cstdlib>
+
+void print_usage() {
+    std::cerr << "Usage: ChessGame [options]" << std::endl;
+    std::cerr << "Options:" << std::endl;
+    std::cerr << "  --verbose, --debug, -verbose, -debug, -v, -V, -d, -D : Show all details and debug information." << std::endl;
+    std::cerr << "  --test, -test, -t, -T <filename> : Test the program with the specified input file." << std::endl;
+    std::cerr << "  --ai, --bot, -ai, -bot <AI_name> : Play against the specified AI." << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,50 @@
 #include <../include/main.h>
 
-int main(int argc, char* charv []) {
+int main(int argc, char* argv []) {
+    int verbose = 0;
+    std::string testFile;
+    std::string aiName;
 
+    const char *const short_opts = "vVdDt:T:a:";
+    const option long_opts[] = {
+        {"verbose", no_argument, nullptr, 'v'},
+        {"debug", no_argument, nullptr, 'd'},
+        {"test", required_argument, nullptr, 't'},
+        {"bot", required_argument, nullptr, 'a'},
+        {"ai", required_argument, nullptr, 'a'},
+        {nullptr, 0, nullptr, 0}
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, short_opts, long_opts, nullptr)) != -1) {
+        switch (opt) {
+            case 'v':
+            case 'V':
+            case 'd':
+            case 'D':
+                verbose = 1;
+                break;
+            case 't':
+            case 'T':
+                testFile = optarg;
+                break;
+            case 'a':
+                aiName = optarg;
+                break;
+            default:
+                print_usage();
+        }
+    }
+
+    if (verbose) {
+        if (!testFile.empty())
+            std::cout << "Test File: " << testFile << std::endl;
+        if (!aiName.empty())
+            std::cout << "AI Name: " << aiName << std::endl;
+    }
+
+    std::cout << "Verbose: " << verbose << std::endl;
+
+
+    return 0;
 }
