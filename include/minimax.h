0216b6955cb40d9bdd43f8ea362cc8a75466882d
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2024-03-24 22:19:53 -0700

    Added Format

--- include/minimax.h
+++ include/minimax.h
@@ -5,14 +5,14 @@
 
 #include <../include/chess.h>
 
-std::pair<Coordinates, Coordinates> Darwin(Chessboard& board, Movemaps Map, int depth, bool White);
+std::pair<Coordinates, Coordinates> Darwin(Chessboard &board, Movemaps Map, int depth, bool White);
 
-int Minimax(Chessboard& board, Movemaps Map, int d, bool White);
+int Minimax(Chessboard &board, Movemaps Map, int d, bool White);
 
 using valueMap = std::map<std::pair<Coordinates, Coordinates>, int>;
 
-std::pair<Coordinates, Coordinates> findMax(valueMap& valMap);
+std::pair<Coordinates, Coordinates> findMax(valueMap &valMap);
 
-std::pair<Coordinates, Coordinates> findMin(valueMap& valMap);
+std::pair<Coordinates, Coordinates> findMin(valueMap &valMap);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,27 @@
 #include <../include/main.h>
 
-int main(int argc, char* argv []) {
+int main(int argc, char *argv[]) {
     int verbose = 0;
     std::string testFile;
     std::string aiName;
 
     const char *const short_opts = "vVdDt:T:a:";
-    const option long_opts[] = {
-        {"verbose", no_argument, nullptr, 'v'},
-        {"debug", no_argument, nullptr, 'd'},
-        {"test", required_argument, nullptr, 't'},
-        {"bot", required_argument, nullptr, 'a'},
-        {"ai", required_argument, nullptr, 'a'},
-        {nullptr, 0, nullptr, 0}
-    };
+    const option long_opts[] = { { "verbose", no_argument, nullptr, 'v' },
+        { "debug", no_argument, nullptr, 'd' }, { "test", required_argument, nullptr, 't' },
+        { "bot", required_argument, nullptr, 'a' }, { "ai", required_argument, nullptr, 'a' },
+        { nullptr, 0, nullptr, 0 } };
 
     int opt;
     while ((opt = getopt_long(argc, argv, short_opts, long_opts, nullptr)) != -1) {
         switch (opt) {
-            case 'v':
-            case 'V':
-            case 'd':
-            case 'D':
-                verbose = 1;
-                break;
-            case 't':
-            case 'T':
-                testFile = optarg;
-                break;
-            case 'a':
-                aiName = optarg;
-                break;
-            default:
-                print_usage();
+        case 'v':
+        case 'V':
+        case 'd':
+        case 'D': verbose = 1; break;
+        case 't':
+        case 'T': testFile = optarg; break;
+        case 'a': aiName = optarg; break;
+        default: print_usage();
         }
     }
 
@@ -61,12 +50,13 @@ int main(int argc, char* argv []) {
         getInput(origin);
         if (origin == "UNDO")
             continue;
-        
+
         Coordinates o = AddresstoIndex(origin);
 
-        if ((White && Board[o.first][o.second].identity <= 0) || (!White && Board[o.first][o.second].identity >= 0)) { 
+        if ((White && Board[o.first][o.second].identity <= 0)
+            || (!White && Board[o.first][o.second].identity >= 0)) {
             playerSelect = true;
-            continue; 
+            continue;
         }
 
         playerSelect = false;
